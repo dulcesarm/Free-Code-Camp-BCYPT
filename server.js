8eda9ce9b4e06651cc741bf320fc824c2f6190b7
dulcesarm Sarmiento <dulsarm@gmail.com>
2025-04-09 02:16:59 +0000

    Actualización de archivos Server

--- server.js
+++ server.js
@@ -1,53 +1,27 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
+const bcrypt = require('bcrypt');
 fccTesting(app);
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
 
+// START_ASYNC (dejamos vacío porque esta prueba es sincrónica)
+// END_ASYNC
 
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
+// START_SYNC - coloca el código aquí entre estas notas
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash); // FreeCodeCamp espera que solo imprimas el hash aquí
 
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result); // FreeCodeCamp espera "true" aquí
+// END_SYNC
 
-
-//END_ASYNC
-
-//START_SYNC
-
-
-
-//END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// hiiiiiiiiii
+// sdfghj
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
